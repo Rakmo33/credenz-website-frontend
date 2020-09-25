edc7ac0b3421e380b49ca7e0a300c480afe94f0a
Fatemask <fatemask29@gmail.com>
2020-09-25 12:51:29 +0530

    floating button

--- credenz20/src/Components/HomeComponent/HomeComponent.js
+++ credenz20/src/Components/HomeComponent/HomeComponent.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aboutus from './Aboutus/Aboutus';
 import Sponsors from "./sponsors/sponsors";
 import HomeAnimation from './HomeAnimation/HomeAnimation';
+import SideEvent from '../sideEvent/sideEvent';
 
 const Home=()=> {
     return(
         <div>
+            <SideEvent/>
             <h1>Credenz Home Page</h1>
             <HomeAnimation/>
             <Aboutus/>
