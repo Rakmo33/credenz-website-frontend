e0b87c55dadb52e25f921dade7fedf21a0b14475
Rakmo33 <omkardabir2013@gmail.com>
2020-09-26 16:44:27 +0530

    Registration form

--- credenz20/src/Components/MainComponent.js
+++ credenz20/src/Components/MainComponent.js
@@ -1,54 +1,43 @@
-import React, { Component } from 'react';
-import  Home  from './HomeComponent/HomeComponent';
-import  Events  from './EventsComponent/EventsComponent';
-import  Register  from './RegisterComponent/RegisterComponent';
-import  Contact  from './ContactComponent/ContactComponent';
-import NavBar from './Navbar/Navbar';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+import Home from "./HomeComponent/HomeComponent";
+import Events from "./EventsComponent/EventsComponent";
+import Register from "./RegisterComponent/RegisterComponent";
+import Contact from "./ContactComponent/ContactComponent";
+import NavBar from "./Navbar/Navbar";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
+  render() {
+    const HomePage = () => {
+      return <Home />;
+    };
 
-    render() {
+    const EventsPage = () => {
+      return <Events />;
+    };
 
-        const HomePage = () => {
-            return(
-                <Home/>
-            );
-        }
+    const RegisterPage = () => {
+      return <Register />;
+    };
 
-        const EventsPage = () => {
-            return(
-                <Events/>
-            );
-        }
+    const ContactPage = () => {
+      return <Contact />;
+    };
 
-        const RegisterPage = () => {
-            return(
-                <Register/>
-            );
-        }
-
-        const ContactPage = () => {
-            return(
-                <Contact/>
-            );
-        }
-
-
-        return(
-            <div>
-                <Switch>
-                    <Route path="/home" component = { HomePage } />
-                    <Route path="/events" component = { EventsPage } />
-                    <Route path="/register" component = { RegisterPage } />
-                    <Route path="/contact" component = { ContactPage } />
-                    <Redirect to="/home"/>
-                </Switch>
-            </div>
-            
-        );
-    }
-
+    return (
+      <div>
+        <Switch>
+          <Route path='/home' component={HomePage} />
+          <Route path='/events' component={EventsPage} />
+          <Route path='/register' component={RegisterPage} />
+          <Route path='/contact' component={ContactPage} />
+          <Redirect to='/home' />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Main;
