6637e2d673d60693356e90e3841f69d99b80092c
Fatemask <fatemask29@gmail.com>
2020-09-30 20:28:51 +0530

    nav

--- credenz20/src/Components/Navbar/Navbar.js
+++ credenz20/src/Components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css'
-
+import Modal1 from '../Modal/Modal';
 
 function NavBar() {
        //const {isShowing, toggle} = useModal();
