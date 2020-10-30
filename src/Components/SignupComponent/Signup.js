def22560507d94ce12c367466f61133cbbc5750c
Sakshee <sakshee1120@gmail.com>
2020-10-30 23:26:14 +0530

    Sign up API

--- src/Components/SignupComponent/Signup.js
+++ src/Components/SignupComponent/Signup.js
@@ -2,22 +2,88 @@ import React, { useState } from 'react';
 import './Signup.css';
 import Footer from '../Footer/footer';
 import Social from '../sideEventButton/sideEvent';
+import Validate from './ValidateInfo';
+import axios from 'axios';
 
 function Signup() {
-
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [phone, setPhone] = useState('')
     const [college, setCollege] = useState('')
     const [password, setPassword] = useState('')
+    const [passwordCheck, setPasswordCheck] = useState('')
+    
 
     const submitForm = e => {
-        alert("Current state is : " + JSON.stringify({email, phone, college, password}))
-        e.preventDefualt()
+        console.log("Current state is : " + JSON.stringify({email, phone, college, password}))
+
+
+        if(!errors.boolean) {
+
+            alert("Inside if")
+            
+            alert("Form successfully submitted.")
+    
+    
+                try{
+
+                    alert("Inside Try")
+
+                    axios.post('http://credenzwebsite.herokuapp.com/signup', {
+                        username: username,
+                        name: name,
+                        password: password,
+                        email: email,
+                        phoneno: phone,
+                        clgname: college,
+                    }).then(function (response) {
+                        alert("Then")
+                        console.log(response);
+                        console.log(response.data);
+                        const msg = response.data["accessToken"]
+                        alert(msg)
+                    })
+                }
+                catch(e) {
+                    alert("Axios error!" + e)
+                }
+
+                setName('')
+                setUsername('')
+                setPhone('')
+                setEmail('')
+                setCollege('')
+                setPassword('')
+               
+            }
+
+           // e.preventDefualt()
+
     }
+
+        const values = {
+            username: username,
+            name: name,
+            password: password,
+            email: email,
+            phoneno: phone,
+            clgname: college,
+            passwordCheck: passwordCheck
+        }
+        
+        console.log(values)
+    
+        const errors = Validate(values)
+            
+        console.log("Errors: " + errors.name + errors.username + errors.email + errors.phone)
+
+    //e.preventDefualt()
+
+
 
     return(
         <>
-        
 
         <div className="container">
             <div className="row py-5 mt-4 align-items-center">
@@ -31,25 +97,39 @@ function Signup() {
                     <form onSubmit={submitForm}>
                         <div className="row">
 
-                            <div className="input-group col-lg-6 mb-4">
+                            <div className="input-group col-lg-12 mb-4">
                                 <div className="input-group-prepend">
                                     <span className="input-group-text bg-white px-4 border-md border-right-0">
                                         <i className="fa fa-user text-muted"></i>
                                     </span>
                                 </div>
-                                <input id="firstName" type="text" name="firstname" placeholder="First Name" class="form-control bg-white border-left-0 border-md"/>
+                                <input id="Name" type="text" name="name" placeholder="Name" class="form-control bg-white border-left-0 border-md"
+                                onChange={ e => setName(e.target.value) }/>
+                                 { 
+                                    errors &&
+                                    <small id="nameErr" class="form-text text-danger">
+                                        {errors.name}
+                                    </small>  
+                                 }
                             </div>
 
-                
-                            <div class="input-group col-lg-6 mb-4">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text bg-white px-4 border-md border-right-0">
-                                        <i class="fa fa-user text-muted"></i>
+
+                            <div className="input-group col-lg-12 mb-4">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text bg-white px-4 border-md border-right-0">
+                                        <i className="fa fa-user text-muted"></i>
                                     </span>
                                 </div>
-                                <input id="lastName" type="text" name="lastname" placeholder="Last Name" class="form-control bg-white border-left-0 border-md"/>
+                                <input id="username" type="text" name="username" placeholder="Username" class="form-control bg-white border-left-0 border-md"
+                                onChange={ e => setUsername(e.target.value) }/>
+                                 { 
+                                    errors &&
+                                    <small id="nameErr" class="form-text inline text-danger">
+                                        {errors.username}
+                                    </small>  
+                                 }
                             </div>
-
+                
                             <div class="input-group col-lg-12 mb-4">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text bg-white px-4 border-md border-right-0">
@@ -59,18 +139,29 @@ function Signup() {
                                 <input id="email" type="email" name="email" placeholder="Email Address"
                                  class="form-control bg-white border-left-0 border-md"
                                  onChange={ e => setEmail(e.target.value) }/>
+                                 { 
+                                    errors &&
+                                    <small id="nameErr" class="form-text text-danger">
+                                        {errors.email}
+                                    </small>  
+                                 }
                             </div>
 
-                            
                             <div class="input-group col-lg-12 mb-4">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text bg-white px-4 border-md border-right-0">
                                         <i class="fa fa-phone-square text-muted"></i>
                                     </span>
                                 </div>
-                                <input id="phoneNumber" type="tel" name="phone" placeholder="Phone Number"
+                                <input id="phoneNumber" type="text" name="phone" placeholder="Phone Number"
                                  class="form-control bg-white border-md border-left-0 pl-3"
                                  onChange={ e => setPhone(e.target.value) }/>
+                                 { 
+                                    errors &&
+                                    <small id="phoneErr" class="form-text text-danger">
+                                        {errors.phone}
+                                    </small>  
+                                 }
                             </div>
 
                             <div class="input-group col-lg-12 mb-4">
@@ -82,6 +173,12 @@ function Signup() {
                                 <input id="college" type="college" name="college" placeholder="College"
                                  class="form-control bg-white border-left-0 border-md"
                                  onChange={ e => setCollege(e.target.value) }/>
+                                 { 
+                                    errors &&
+                                    <small id="clgErr" class="form-text text-danger">
+                                        {errors.college}
+                                    </small>  
+                                 }
                             </div>
 
                             <div class="input-group col-lg-6 mb-4">
@@ -102,9 +199,17 @@ function Signup() {
                                         <i class="fa fa-lock text-muted"></i>
                                     </span>
                                 </div>
-                                <input id="passwordConfirmation" type="text" name="passwordConfirmation" placeholder="Confirm Password" class="form-control bg-white border-left-0 border-md"/>
+                                <input id="passwordConfirmation" type="password" name="passwordConfirmation" placeholder="Confirm Password" class="form-control bg-white border-left-0 border-md"/>
                             </div>
 
+                            <div class="form-group col-lg-12 mx-auto mb-0">
+                            { 
+                                    errors &&
+                                    <small id="passwordErr" class="form-text text-danger">
+                                        {errors.password}
+                                    </small>  
+                            }
+                            </div>
                     
                             <div class="form-group col-lg-12 mx-auto mb-0">
                                 <button type="submit" class="btn btn-primary btn-block py-2">
@@ -131,35 +236,3 @@ function Signup() {
 }
 
 export default Signup;
-
-
- /*
- <div className="page">
-            <div  className="container">
-                <div className="left">
-                    <div className="signup">Sign Up</div>
-                    <div className="text-left">Sign up here and be a part of the exciting adventure Credenz 2020 is!</div>
-                </div>
-                <div className="right">
-                    {<svg viewBox="0 0 320 300">
-                        <path d="m 40,120.00016 239.99984,-3.2e-4 c 0,0 24.99263,0.79932 25.00016,35.00016 0.008,34.20084 -25.00016,35 -25.00016,35 h -239.99984 c 0,-0.0205 -25,4.01348 -25,38.5 0,34.48652 25,38.5 25,38.5 h 215 c 0,0 20,-0.99604 20,-25 0,-24.00396 -20,-25 -20,-25 h -190 c 0,0 -20,1.71033 -20,25 0,24.00396 20,25 20,25 h 168.57143" />
-                    </svg>}
-                    <form className="form" onSubmit = {submitForm}>
-                        <label for="email">Email</label>
-                        <input type="email" id="email" onChange={ e => setEmail(e.target.value) }/>
-                        <label for="text">User name</label>
-                        <input type="text" id="username" onChange={ e => setUsername(e.target.value) }/>
-                        <label for="text">Phone</label>
-                        <input type="text" id="phone" onChange={ e => setPhone(e.target.value) }/>
-                        <label for="text">College</label>
-                        <input type="text" id="college" onChange={ e => setCollege(e.target.value) }/>
-                        <label for="password">Password</label>
-                        <input type="password" id="password"
-                        onChange={ e => setPassword(e.target.value) }/>
-                        <input type="submit" id="submit" value="Submit"/>
-                        {<button id="submit" type="submit" class="btn btn-secondary btn-sm">Submit</button>}
-                    </form>
-                </div>
-            </div>
-        </div>   
-*/ 
