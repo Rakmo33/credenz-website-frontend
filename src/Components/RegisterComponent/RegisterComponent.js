84560af4bdfeb0d08d4537583577b45b9339f3ee
Sakshee <sakshee1120@gmail.com>
2020-11-20 22:49:35 +0530

    Log out conditions

--- src/Components/RegisterComponent/RegisterComponent.js
+++ src/Components/RegisterComponent/RegisterComponent.js
@@ -16,99 +16,78 @@ import EventWrap from "./EventWrap";
 import Proceed from "./Proceed";
 import NavigateButton from "./NavigateButton";
 import jwt_decode from "jwt-decode";
-import jwt from "jwt-decode";
+import jwt from 'jwt-decode'
+
 
 const Register = () => {
   // for total price
   const [total, setTotal] = useState(0);
-  let user = "";
-
-  if (localStorage.getItem("user")) {
-    user = jwt(localStorage.getItem("user"));
-  }
-
-  const defaultFormData = {
-    name1: JSON.stringify(user["name"]).replace(/"/g, ""),
-    name2: "",
-    name3: "",
-    name4: "",
-    year: undefined,
-    number1: JSON.stringify(user["phoneno"]).replace(/"/g, ""),
-    number2: undefined,
-    email1: JSON.stringify(user["email"]).replace(/"/g, ""),
-    email2: "",
-    isMember: undefined,
-    college: JSON.stringify(user["clgname"]).replace(/"/g, ""),
-    events: {},
-  };
-
-  // total number of events
-  // const totalEvents = 9;
   const [events, setEvents] = useState([
     {
       id: "event1",
       name: "Clash",
       username: "clash",
       price: 100,
-      isChecked: false,
+      isCheked: false,
     },
     {
       id: "event2",
       name: "Reverse Coding",
       username: "rc",
       price: 50,
-      isChecked: false,
+      isCheked: false,
     },
     {
       id: "event3",
       name: "Pixelate",
       username: "pixelate",
       price: 50,
-      isChecked: false,
+      isCheked: false,
     },
     {
       id: "event4",
       name: "Cretronix",
       username: "cretronix",
       price: 50,
-      isChecked: false,
+      isCheked: false,
     },
     {
       id: "event5",
       name: "Bplan",
       username: "bplan",
       price: 50,
-      isChecked: false,
+      isCheked: false,
     },
     {
       id: "event6",
       name: "Wallstreet",
       username: "wallstreet",
       price: 50,
-      isChecked: false,
+      isCheked: false,
     },
     {
       id: "event7",
       name: "Roboliga",
       username: "roboliga",
       price: 50,
-      isChecked: false,
+      isCheked: false,
     },
     {
       id: "event8",
       name: "Enigma",
       username: "enigma",
       price: 50,
-      isChecked: false,
+      isCheked: false,
     },
     {
       id: "event9",
       name: "Quiz",
       username: "quiz",
       price: 50,
-      isChecked: false,
+      isCheked: false,
     },
   ]);
+
 
       let user = ""
         //alert("JUST BEFORE IF")
@@ -166,144 +145,6 @@ const Register = () => {
         if (!tempEvents[eventNum].isCheked) {
           tempEvents[eventNum].isCheked = true;
           setEvents(tempEvents);
-
-  const changeHandler = (eventNum) => {
-    let tempEvents = [...events];
-
-    // When Checked
-    if (!tempEvents[eventNum].isChecked) {
-      tempEvents[eventNum].isChecked = true;
-      setEvents(tempEvents);
-
-      setTotal((total) => total + tempEvents[eventNum].price);
-    }
-    // When Unchecked
-    else {
-      tempEvents[eventNum].isChecked = false;
-      setEvents(tempEvents);
-
-      setTotal((total) => total - tempEvents[eventNum].price);
-    }
-  };
-
-  const [formData, setFormData] = useForm(defaultFormData);
-
-  // disable buttons
-  const [showPrevBtn, setPrev] = useState(false);
-  const [showNextBtn, setNext] = useState(true);
-
-  // display only first component
-  const [isVisible, setVisible] = useState([
-    true,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ]);
-  const [anim, setAnim] = useState(["", "", "", "", "", ""]);
-
-  // next click
-  const nextHandler = () => {
-    // find index of visible component
-    const index = isVisible.findIndex((item) => item === true);
-    if (index === 0) {
-      if (formData.name1 === "") {
-        document.querySelector("#Name1").classList.add("err");
-        document.querySelector("span#Name1").innerHTML =
-          "This Field Is Required!";
-        return;
-      }
-    }
-    if (index === 1) {
-      if (formData.year === undefined) {
-        document.querySelector("#Radio1").classList.add("err");
-        document.querySelector("span#yearErrMsg").innerHTML =
-          "Please Select A Category!";
-        document.querySelector("span#yearErrMsg").classList.add("txt-center");
-        return;
-      }
-    }
-    let err = false;
-    if (index === 2) {
-      if (!/^[6789]\d{9}$/.test(formData.number1)) {
-        document.querySelector("span#inputContact1").innerHTML =
-          "Enter Valid Contact Number!";
-        document.querySelector("#inputContact1").classList.add("err");
-        err = true;
-      } else {
-        document.querySelector("span#inputContact1").innerHTML = "";
-        document.querySelector("#inputContact1").classList.remove("err");
-        err = false;
-      }
-      if (!/^[6789]\d{9}$/.test(formData.number2)) {
-        document.querySelector("span#inputContact2").innerHTML =
-          "Enter Valid Contact Number!";
-        document.querySelector("#inputContact2").classList.add("err");
-        err = true;
-      } else {
-        document.querySelector("span#inputContact2").innerHTML = "";
-        document.querySelector("#inputContact2").classList.remove("err");
-        err = false;
-      }
-      if (
-        !/^\w+[\S\w+]*@[a-zA-Z]+\.([a-zA-Z]+\.)?[a-zA-Z]+$/.test(
-          formData.email1
-        )
-      ) {
-        document.querySelector("span#inputEmail1").innerHTML =
-          "Enter Valid Email!";
-        document.querySelector("#inputEmail1").classList.add("err");
-        err = true;
-      } else {
-        document.querySelector("span#inputEmail1").innerHTML = "";
-        document.querySelector("#inputEmail1").classList.remove("err");
-        err = false;
-      }
-      if (
-        !/^\w+[\S\w+]*@[a-zA-Z]+\.([a-zA-Z]+\.)?[a-zA-Z]+$/.test(
-          formData.email2
-        )
-      ) {
-        document.querySelector("span#inputEmail2").innerHTML =
-          "Enter Valid Email!";
-        document.querySelector("#inputEmail2").classList.add("err");
-        err = true;
-      } else {
-        document.querySelector("span#inputEmail2").innerHTML = "";
-        document.querySelector("#inputEmail2").classList.remove("err");
-        err = false;
-      }
-      if (err) return;
-    }
-    if (index === 3) {
-      if (formData.isMember === undefined) {
-        document.querySelector("span#memberErrMsg").innerHTML =
-          "Please Choose An Option!";
-        document.querySelector("span#memberErrMsg").classList.add("txt-center");
-        err = true;
-      } else {
-        document.querySelector("span#memberErrMsg").innerHTML = "";
-        err = false;
-      }
-      if (formData.college === undefined) {
-        document.querySelector("span#clgErrMsg").innerHTML =
-          "Please Select Your College";
-        document.querySelector("span#clgErrMsg").classList.add("txt-center");
-        err = true;
-      } else {
-        document.querySelector("span#clgErrMsg").innerHTML = "";
-        err = false;
-      }
-      if (err) return;
-    }
-    if (index === 4) {
-      err = true;
-      for (var e in formData.events) {
-        // if at least one event is selected
-        if (formData.events[e]) {
-          err = false;
-          break;
         }
       };
     
@@ -586,48 +427,6 @@ const Register = () => {
 
         document.body.appendChild(script);
       });
-
-      if (err) {
-        document.querySelector("span#eventErrMsg").innerHTML =
-          "Please Select At Least One Event!";
-        return;
-      } else {
-        document.querySelector("span#eventErrMsg").innerHTML = "";
-      }
-      // alert("LOL");
-    }
-    // disable button for first component
-    setPrev(true);
-    setNext(true);
-    if (index === 4) {
-      setNext(false);
-    }
-    if (index === 5) {
-      return;
-    }
-    let tempVisible = [...isVisible];
-    // hide current
-    tempVisible[index] = false;
-    // display next
-    tempVisible[index + 1] = true;
-    // set animations
-    let tempAnim = ["", "", "", "", "", ""];
-    tempAnim[index] = "toLeft";
-    tempAnim[index + 1] = "fromRight";
-    setAnim(tempAnim);
-    // timeout for completion of animations before disappearing
-    setTimeout(() => setVisible(tempVisible), 200);
-  };
-
-  // prev click
-  const prevHandler = () => {
-    // find index of current
-    const index = isVisible.findIndex((item) => item === true);
-    setPrev(true);
-    setNext(true);
-
-    if (index === 1) {
-      setPrev(false);
     }
         return (
           <div>
@@ -720,8 +519,6 @@ const Register = () => {
 
       
    }
-
-   
 
 
 
