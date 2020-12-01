ebfec1c2136f1e15bb15b25744fa59a54fd04319
Fatemask <fatemask29@gmail.com>
2020-12-01 20:44:27 +0530

    footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -2,19 +2,27 @@ import React from 'react';
 import classes from './footer.module.css';
 import "../ContactComponent/contact.css";
 import {Link} from 'react-router-dom';
+import {Row, Col, Container} from 'react-bootstrap';
 
 const footer=()=>{
     return(
     <div className={classes.footer} >
-        <div><span className={'footer-font'}>copyright @ PICT IEEE Student Branch</span></div>
-        <div>
+        <Container style={{textAlign:"center"}}>
+        <Row>
+            <span className={'footer-font'}>copyright @ PICT IEEE Student Branch</span>
+        </Row>
+        
+        <Row>
             <div className={classes.social}>
             <a className={classes.iconfb} href="#"> <i className={"fa fa-facebook"}></i></a>
             <a className={classes.iconlink} href="#"> <i className={"fa fa-linkedin"}></i></a>
             <a className={classes.iconinsta} href="#"> <i className={"fa fa-instagram"}></i></a>
             </div>
-            <Link to="/team"><button className={"team btn"}>OUR TEAM</button></Link>
-        </div>
+        </Row>
+        <Row>
+            Developed with &#9829; by PISB &nbsp; <Link to="/team"><span style={{color:"#6eabce"}}> Web Team</span></Link>
+        </Row>
+        </Container>
     </div>
     );
 }
