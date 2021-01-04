29f416fee47d4b941a456dd126b8bb4effc1884a
Fatemask <fatemask29@gmail.com>
2021-01-04 17:11:06 +0530

    side event

--- src/Components/SideEvents/sideEventCards.jsx
+++ src/Components/SideEvents/sideEventCards.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import EventCard from "./eventCard";
 import logo from "../../assests/img/bg1.jpg";
+import web1 from '../../assests/img/webweaver1.jpeg';
 
 const SideEventCards = () => {
   return (
     <React-Fragment>
-      <h3 className="event-title title-1">Web Weaver</h3>
+      {/* <h3 className="event-title title-1">Web Weaver</h3> */}
+      <div className="row content" style={{marginTop:'5vh'}}>
+        <EventCard
+          logo={web1}
+          name="Space Cruiser"
+          info={"There is an outbreak of a space ship combat. Enter into your almighty spaceship and destroy the attackers, to be the protector of the universe.The whole hopes of are on you cheif!!"}
+        />
+        <EventCard
+          logo={logo}
+          name="name4"
+          info={"info4. some info about it. play for free"}
+        />
+        {/* <EventCard
+          logo={logo}
+          name="name4"
+          info={"info4. some info about it. play for free"}
+        /> */}
+      </div>
+      {/* <h2 className="event-title">Clash &amp; RC</h2>
       <div className="row content">
         <EventCard
           logo={logo}
@@ -17,25 +36,7 @@ const SideEventCards = () => {
           name="name4"
           info={"info4. some info about it. play for free"}
         />
-        <EventCard
-          logo={logo}
-          name="name4"
-          info={"info4. some info about it. play for free"}
-        />
-      </div>
-      <h2 className="event-title">Clash &amp; RC</h2>
-      <div className="row content">
-        <EventCard
-          logo={logo}
-          name="name4"
-          info={"info4. some info about it. play for free"}
-        />
-        <EventCard
-          logo={logo}
-          name="name4"
-          info={"info4. some info about it. play for free"}
-        />
-      </div>
+      </div> */}
     </React-Fragment>
   );
 };
