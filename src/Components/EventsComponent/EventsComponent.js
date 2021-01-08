cde0b9e2dfa229099a64c90f486af9058946e587
Rakmo33 <omkardabir2013@gmail.com>
2021-01-08 11:08:53 +0530

    robo extra spacing

--- src/Components/EventsComponent/EventsComponent.js
+++ src/Components/EventsComponent/EventsComponent.js
@@ -3,10 +3,9 @@ import SideEvent from "../sideEventButton/sideEvent";
 import Modal1 from "../Modal/Modal";
 import axios from "axios";
 import Animation from "./Animation";
-// import { EVENTS } from "./allEvents";
 import Footer from "../Footer/footer";
 import { Nav } from "react-bootstrap";
-import Navbar from '../Navbar/Navbar';
+import Navbar from "../Navbar/Navbar";
 
 const Events = () => {
   // window.location.reload(false); //refresh page
@@ -41,29 +40,13 @@ const Events = () => {
     });
   }, []);
 
-  //console.log("Events"+ JSON.stringify(EVENTS))
-
   let type = false;
 
   return (
     <div className='evePage' style={{ marginTop: "70px" }}>
-      <Navbar/>
+      <Navbar />
       <SideEvent />
-
-      {/* <h1 style={{ color: "ivory" }}>Events Page</h1> */}
-      {/*<ul style={{ color: "ivory" }}>
-        <li onClick={() => handleShow("clash")}>Clash</li>
-        <li onClick={() => handleShow("rc")}>Reverse Coding</li>
-        <li onClick={() => handleShow("enigma")}>Enigma</li>
-        <li onClick={() => handleShow("xodia")}>Xodia</li>
-        <li onClick={() => handleShow("webweaver")}>Webweaver</li>
-        <li onClick={() => handleShow("bplan")}>BPlan</li>
-        <li onClick={() => handleShow("quiz")}>Quiz</li>
-      </ul>*/}
-      {/*<Robo/>*/}
-
       <Animation></Animation>
-
       <Modal1
         show={show}
         handleClose={handleClose}
