d75e0583ddd239bc02a2cc6bd263e4487f774b2e
Sakshee <sakshee1120@gmail.com>
2021-02-04 13:42:52 +0530

    PAyment DONE

--- src/Components/Ipay/Ipay.js
+++ src/Components/Ipay/Ipay.js
@@ -1,45 +1,19 @@
 import React, {Component} from 'react';
 import {Ippopay} from 'react-ippopay';
-import axios from 'axios';
-
-function getOrderID(amount){
-
-    console.log("get order id")
-
-    axios({
-        method: "post",
-        url: `${process.env.REACT_APP_API_URL}/payment`,
-        body: {
-            username: "sakshee",
-            amount: "120",
-            email: "sakshee1120@gmail.com",
-            phoneno: "9420324462"
-        },
-        headers:{
-            'Content-Type': 'application/json',
-        }
-      }).then((response)=>{
-          alert(JSON.stringify(response.data))
-          console.log(JSON.stringify(response.data))
-      }).catch((err)=>{
-          console.log("error"+err)
-      })
-}
-
-/*username: 'pk_live_LxGuVR5mwBuy',
-            password: 'sk_live_eaSKsfOhheBG4PiKerw9ihnwLc84WYLKc2I9IL4u'*/
+import jwt from "jwt-decode";
 
 class Ipay extends Component {
   
     state = {
     ippopayOpen: false,
-    order_id:'order_T4AoEoBVt',
-    public_key:'pk_live_LxGuVR5mwBuy'
+    order_id:'',
+    public_key: 'pk_live_LxGuVR5mwBuy'
   };
 
   ippopayHandler(e){
     if(e.data.status === 'success'){
-        console.log("success" + e.data)
+        console.log("success" + e.data);
+        this.props.Register();
     }
     if(e.data.status === 'failure'){
         console.log("failure" + e.data)
@@ -48,25 +22,64 @@ class Ipay extends Component {
   componentDidUpdate(){
     window.addEventListener('message', this.ippopayHandler);
   }
-
   componentDidMount(){
-    getOrderID(this.props.sum);
+    let user = "";
+    if (localStorage.getItem("user")) {
+      user = jwt(localStorage.getItem("user"));
+    }
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        username: localStorage.getItem("user") ? JSON.stringify(user["username"]).replace(/"/g, "") : '',
+        amount: this.props.sum,
+        email: localStorage.getItem("user") ? JSON.stringify(user["email"]).replace(/"/g, "") : '',
+        phoneno: localStorage.getItem("user") ? JSON.stringify(user["phoneno"]).replace(/"/g, "") : ''
+       })
+    };
+
+    fetch(`${process.env.REACT_APP_API_URL}/payment`, requestOptions)
+        .then(response => {
+          //alert(JSON.stringify(response))
+          return response.json()
+        })
+        .then(data => {
+          //alert(JSON.stringify(data.data.order.order_id))
+          //alert("orderid" + data)
+          console.log(data)
+          this.setState({order_id: data.data.order.order_id})
+        });
+
+  //          alert("orderid" + this.props.order_id)
   }
 
   ippopayOpen(){
     this.setState({ippopayOpen: true});
   }
+
+  
   render() {
 
-    return (
-      <div>
-          
-        <span onClick={e => this.ippopayOpen(e)}>Proceed to pay Rs {this.props.sum}</span>
-        <Ippopay ippopayOpen={this.state.ippopayOpen} ippopayClose={true} order_id={this.state.order_id} public_key={this.state.public_key}/>
-        {/*<button onClick={()=>{getOrderID(this.props.sum)}}>pay</button>*/}
-      </div> 
-    );
+    if(this.state.order_id!=='' || this.state.order_id!==undefined) {
+      console.log(this.state.order_id)
+      return (
+        
+        <div>
+          <span onClick={e => this.ippopayOpen(e)}>Proceed to pay Rs {this.props.sum}</span>
+          <Ippopay ippopayOpen={this.state.ippopayOpen} ippopayClose={true} order_id={this.state.order_id} public_key={this.state.public_key}/>
+        </div> 
+      );
+      
+    }else {
+      return(
+        <div>
+          Payment request being processed
+        </div>
+      ); 
+    }
+    
   }
 }
+
 
 export default Ipay;
